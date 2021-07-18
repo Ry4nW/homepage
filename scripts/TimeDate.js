8c97b371cb2d57e02f2749922aaebc9fe03df53c
Ryan <ryanbcwang@gmail.com>
2021-07-17 21:12:22 -0700

    feat: timed backgrounds, adjusted search

--- scripts/TimeDate.js
+++ scripts/TimeDate.js
@@ -1,26 +1,56 @@
 const date = new Date();
 
+let h = (date.getHours() < 10 ? '0' : '') + date.getHours();
+
 function TimeDate() {
 
-    let timeString = new Date(new Date().getTime() + 4).toLocaleTimeString();
     let greetingString;
+    let timeString = new Date(new Date().getTime() + 4).toLocaleTimeString();
 
-    let h = (date.getHours() < 10 ? '0' : '') + date.getHours()
-
-    if (h >= 21)
-        greetingString = 'Good night, '
-    else if (h >= 18)
-        greetingString = 'Good evening, '
-    else if (h >= 12) {
-        greetingString = 'Good afternoon, '
+    if (h >= 21) {
+        greetingString = 'Good night, ';
+    } else if (h >= 18) {
+        greetingString = 'Good evening, ';
+    } else if (h >= 12) {
+        greetingString = 'Good afternoon, ';
     } else {
-        greetingString = 'Good morning, '
+        greetingString = 'Good morning, ';
     }
+
     document.getElementById('theTime').innerText = timeString;
     document.getElementById('theGreeting').innerText = `${greetingString}Ryan.`;
+
 }
 
 setInterval(TimeDate, 1);
+
+function setBackground() {
+
+    const imageRandomizer = Math.floor(Math.random() * 4) + 1;
+
+    if (parseInt(h) >= 21) {
+        document.body.style.backgroundImage = `url(./backgrounds/night/${imageRandomizer}.jpg)`;
+
+    } else if (parseInt(h) >= 18) {
+
+        const folders = ['night', 'day'];
+        const folderRandomizer = Math.floor(Math.random());
+        const folderString = folders[folderRandomizer];
+
+        document.body.style.backgroundImage = `url('./backgrounds/${folderString}/${imageRandomizer}.jpg')`;
+
+    } else if (parseInt(h) >= 12) {
+        document.body.style.backgroundImage = `url(./backgrounds/day/${imageRandomizer}.jpg)`;
+
+    } else {
+        document.body.style.backgroundImage = `url(./backgrounds/morning/${imageRandomizer}.jpg)`;
+
+    }
+
+    document.body.style.backgroundSize = 'cover'; // This will make sure that our
+
+}
+setBackground();
 
 function calendar() {
 
@@ -35,3 +65,4 @@ function calendar() {
     document.getElementById('theDate').innerText = `${d}, ${m} ${n}, ${y}`;
 }
 setInterval(calendar, 1);
+
